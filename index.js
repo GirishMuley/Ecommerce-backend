7566d4d0dc305742117d3e2ffab726fa8b40c92b
Girish Muley <girish.muley007@gmail.com>
2023-12-30 17:15:44 +0530

    Passport js implemented with local and jwt

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const server = express();
 var cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const productsRouters = require("./routes/Products");
 const categoriesRouter = require("./routes/Categorys");
@@ -9,20 +16,108 @@ const usersRouter = require("./routes/Users");
 const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const orderRouter = require("./routes/Orders");
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser } = require("./services/common");
+
+const SECRET_KEY = "SECRET_KEY";
+
+//JWT options
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY; //TODO: should not be in code
+
 //middlewares
+
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate("session"));
+
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
 server.use(express.json()); // to parse req.body
-server.use("/products", productsRouters.router);
-server.use("/categories", categoriesRouter.router);
-server.use("/brands", brandRouter.router);
-server.use("/users", usersRouter.router);
+server.use("/products", isAuth(), productsRouters.router); //we can also use JWT token
+server.use("/categories", isAuth(), categoriesRouter.router);
+server.use("/brands", isAuth(), brandRouter.router);
+server.use("/users", isAuth(), usersRouter.router);
 server.use("/auth", authRouter.router);
-server.use("/cart", cartRouter.router);
-server.use("/orders", orderRouter.router);
+server.use("/cart", isAuth(), cartRouter.router);
+server.use("/orders", isAuth(), orderRouter.router);
+
+//passport strategies
+passport.use(
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
+    //by default passport uses username
+    try {
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: "invalid credentials" });
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (error, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: "invalid credentials" });
+          }
+          const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+          done(null, token); //this line sends to serializer
+        }
+      );
+    } catch (error) {
+      done(error);
+    }
+  })
+);
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    console.log(jwt_payload);
+    try {
+      const user = await User.findOne({ id: jwt_payload.sub });
+      if (user) {
+        return done(null, sanitizeUser(user));
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    } catch (error) {
+      return done(err, false);
+    }
+  })
+);
+
+//this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+//this changes session variable req.user when called from authorized request
+passport.deserializeUser(function (user, cb) {
+  console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 main().catch((err) => console.log(err));
 
