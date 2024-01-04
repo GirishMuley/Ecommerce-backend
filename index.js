d4a301c7e00d8ce9afe33d5844730a2f7bc04100
Girish Muley <girish.muley007@gmail.com>
2024-01-04 15:06:09 +0530

    stripe integration

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ server.use(
     exposedHeaders: ["X-Total-Count"],
   })
 );
+server.use(express.raw({ type: "application/json" }));
 server.use(express.json()); // to parse req.body
 server.use("/products", isAuth(), productsRouters.router); //we can also use JWT token
 server.use("/categories", isAuth(), categoriesRouter.router);
@@ -120,6 +121,67 @@ passport.deserializeUser(function (user, cb) {
     return cb(null, user);
   });
 });
+
+//Payments
+const stripe = require("stripe")(
+  "sk_test_51OUh1ESAINVALbCFEq28cXZYCtw3AI3XCyturlyDxal4XSFhQzIRCZskzH27JFnOzHsVeIuvI0D8cjKmA9O9ZpaU00nNimt9v6"
+);
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100, // for decimal compensation
+    currency: "inr",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Webhook
+
+// TODO: we will capture actual order after deploying out server live on public URL
+
+const endpointSecret =
+  "whsec_b3c0dcb8e10c8f03427412e82a4e8049262de0cb2b90635c9961a218cbfa480f";
+
+server.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        console.log({ paymentIntentSucceeded });
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 main().catch((err) => console.log(err));
 
