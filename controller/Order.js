3346ff67cc565ff1becc18027a7f0cde48b9bb8f
Girish Muley <girish.muley007@gmail.com>
2026-09-22 16:57:19 +0530

    fix order sort issues

--- controller/Order.js
+++ controller/Order.js
@@ -3,160 +3,90 @@ const { Product } = require("../model/Product");
 const { User } = require("../model/User");
 const { sendMail, invoiceTemplate } = require("../services/common");
 
-const formatOrder = (order) => {
-  const orderObject = order.toObject ? order.toObject() : order;
-
-  const { _id, ...rest } = orderObject;
-
-  return {
-    id: _id,
-    ...rest,
-  };
-};
-
-// Fetch orders for logged-in user
 exports.fetchOrdersByUser = async (req, res) => {
   const { id } = req.user;
-
   try {
     const orders = await Order.find({ user: id });
 
-    const formattedOrders = orders.map(formatOrder);
-
-    res.status(200).json(formattedOrders);
+    res.status(200).json(orders);
   } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
+    res.status(400).json(err);
   }
 };
 
-// Create order
 exports.createOrder = async (req, res) => {
+  const order = new Order(req.body);
+  // here we have to update stocks;
+
+  for (let item of order.items) {
+    let product = await Product.findOne({ _id: item.product.id });
+    product.$inc("stock", -1 * item.quantity);
+    // for optimum performance we should make inventory outside of product.
+    await product.save();
+  }
+
   try {
-    const order = new Order(req.body);
-
-    // Update product stock
-    for (let item of order.items) {
-      const product = await Product.findOne({
-        _id: item.product.id,
-      });
-
-      if (!product) {
-        return res.status(404).json({
-          message: "Product not found",
-        });
-      }
-
-      product.stock -= item.quantity;
-
-      await product.save();
-    }
-
     const doc = await order.save();
-
     const user = await User.findById(order.user);
-
-    // Send invoice email
+    // we can use await for this also
     sendMail({
       to: user.email,
       html: invoiceTemplate(order),
       subject: "Order Received",
     });
 
-    res.status(201).json(formatOrder(doc));
+    res.status(201).json(doc);
   } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
+    res.status(400).json(err);
   }
 };
 
-// Delete order
 exports.deleteOrder = async (req, res) => {
   const { id } = req.params;
-
   try {
     const order = await Order.findByIdAndDelete(id);
-
-    if (!order) {
-      return res.status(404).json({
-        message: "Order not found",
-      });
-    }
-
-    res.status(200).json(formatOrder(order));
+    res.status(200).json(order);
   } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
+    res.status(400).json(err);
   }
 };
 
-// Update order
 exports.updateOrder = async (req, res) => {
   const { id } = req.params;
-
   try {
     const order = await Order.findByIdAndUpdate(id, req.body, {
       new: true,
-      runValidators: true,
     });
-
-    if (!order) {
-      return res.status(404).json({
-        message: "Order not found",
-      });
-    }
-
-    res.status(200).json(formatOrder(order));
+    res.status(200).json(order);
   } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
+    res.status(400).json(err);
   }
 };
 
-// Fetch all orders
 exports.fetchAllOrders = async (req, res) => {
+  // sort = {_sort:"price",_order="desc"}
+  // pagination = {_page:1,_limit=10}
+  let query = Order.find({ deleted: { $ne: true } });
+  let totalOrdersQuery = Order.find({ deleted: { $ne: true } });
+
+  if (req.query._sort && req.query._order) {
+    query = query.sort({ [req.query._sort]: req.query._order });
+  }
+
+  const totalDocs = await totalOrdersQuery.count().exec();
+  console.log({ totalDocs });
+
+  if (req.query._page && req.query._limit) {
+    const pageSize = req.query._limit;
+    const page = req.query._page;
+    query = query.skip(pageSize * (page - 1)).limit(pageSize);
+  }
+
   try {
-    const condition = {
-      deleted: { $ne: true },
-    };
-
-    let query = Order.find(condition);
-
-    if (req.query._sort) {
-      const sortField = req.query._sort;
-
-      const sortOrder = req.query._order === "desc" ? -1 : 1;
-
-      query = query.sort({
-        [sortField]: sortOrder,
-      });
-    }
-
-    const page = Math.max(parseInt(req.query._page, 10) || 1, 1);
-
-    const limit = Math.min(parseInt(req.query._limit, 10) || 10, 100);
-
-    if (req.query._page && req.query._limit) {
-      query = query.skip((page - 1) * limit).limit(limit);
-    }
-
-    const [totalDocs, docs] = await Promise.all([
-      Order.countDocuments(condition),
-      query.exec(),
-    ]);
-
-    const formattedOrders = docs.map(formatOrder);
-
+    const docs = await query.exec();
     res.set("X-Total-Count", totalDocs);
-
-    res.status(200).json(formattedOrders);
+    res.status(200).json(docs);
   } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
+    res.status(400).json(err);
   }
 };
