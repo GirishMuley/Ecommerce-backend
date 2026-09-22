edac6e16908b0fe4859d7f51d28772b98f664c40
Girish Muley <girish.muley007@gmail.com>
2026-09-22 17:49:51 +0530

    fix some typos

--- controller/Auth.js
+++ controller/Auth.js
@@ -79,11 +79,7 @@ exports.resetPasswordRequest = async (req, res) => {
 
     //also set token in email
 
-    const resetPageLink =
-      "${process.env.HOST_URL}/reset-password?token=" +
-      resetToken +
-      "&email=" +
-      email;
+    const resetPageLink = `${process.env.HOST_URL}/reset-password?token=${resetToken}&email=${email}`;
     const subject = "reset password for e-commerce";
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset 
   Password</p>`;
